fb8fb6fc208647e26236e4a403543d18081410fa
jjxia81 <xiajianjun2011@hotmail.com>
2024-11-01 19:52:02 -0500

    remove gflag, add cli11 as command line parser

--- vipss/main.cpp
+++ vipss/main.cpp
@@ -2,19 +2,19 @@
 //#include <unistd.h>
 #include "src/vipss_unit.hpp"
 // #include <unistd.h>
-#include <gflags/gflags.h>
-
+// #include <gflags/gflags.h>
+#include <CLI/CLI.hpp>
 
 typedef std::chrono::high_resolution_clock Clock;
 using namespace std;
 
-DEFINE_string(input, "", "The input point file name.");
-DEFINE_string(output, "", "The output file name");
-DEFINE_double(lambda, 0.0, "The smooth term for vipss");
-DEFINE_int32(volumeDim, 100, "The volume dimension for surfacing marching cubes");
-DEFINE_int32(surfacing, 1, "Whether generate surface using Nature Neighbor HRBF");
-DEFINE_int32(initPV, 1, "Whether to use partial vipss to init point normals, this method runs faster");
-DEFINE_int32(hard_constraints, 1, "Whether to use normal length as hard constraints");
+// DEFINE_string(input, "", "The input point file name.");
+// DEFINE_string(output, "", "The output file name");
+// DEFINE_double(lambda, 0.0, "The smooth term for vipss");
+// DEFINE_int32(volumeDim, 100, "The volume dimension for surfacing marching cubes");
+// DEFINE_int32(surfacing, 1, "Whether generate surface using Nature Neighbor HRBF");
+// DEFINE_int32(initPV, 1, "Whether to use partial vipss to init point normals, this method runs faster");
+// DEFINE_int32(hard_constraints, 1, "Whether to use normal length as hard constraints");
 
 void SplitPath(const std::string& fullfilename,std::string &filepath);
 void SplitFileName (const std::string& fullfilename,std::string &filepath,std::string &filename,std::string &extname);
@@ -24,28 +24,44 @@ int main(int argc, char** argv)
     VIPSSUnit vipss_unit;
     std::string out_normal_path;
     std::string out_mesh_path;
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    struct 
+    {
+        std::string input;
+        std::string output;
+        double lambda = 0.0;
+        int volumeDim = 100;
+        bool surfacing = true;
+        bool initPV = true;
+        bool hardConstraints = true;
+    }args;
+    
+    // gflags::ParseCommandLineFlags(&argc, &argv, true);
+    CLI::App app{"local vipss Command Line"};
+    app.add_option("-i, --input ", args.input, "input file")->required();
+    app.add_option("-o, --output ", args.output, "output file, if not given , output will be saved in same folder as input");
+    app.add_option("-l, --lambda ", args.lambda, "lambda value");
+    app.add_option("-s, --volume_size ", args.volumeDim, "surface volumeDim");
+    app.add_option("-P, --initPV", args.initPV, "enable or disable partial vipss for normal initialization");
+    app.add_option("-S, --surfacing", args.surfacing, "reconstruct surface or not");
+    app.add_option("-H, --hardConstraints", args.hardConstraints, "use hard constraints for energy optimization");
+    CLI11_PARSE(app, argc, argv);
 
-
-    vipss_unit.hard_constraints_ = FLAGS_hard_constraints;
-    if(FLAGS_input.size() > 0)
+    vipss_unit.hard_constraints_ = args.hardConstraints;
+    if(args.input.size() > 0)
     {
         std::string in_path, in_filename, in_extname;
-        SplitFileName(FLAGS_input, in_path, in_filename, in_extname);
-        vipss_unit.input_data_path_ = FLAGS_input;
+        SplitFileName(args.input, in_path, in_filename, in_extname);
+        vipss_unit.input_data_path_ = args.input;
         vipss_unit.input_data_ext_  = in_extname;
         std::cout << "input path : " << in_path << std::endl;
         std::cout << "in_filename : " << in_filename << std::endl;
         std::cout << "in_extname : " << in_extname << std::endl;
-
-        LocalVipss::InitWithPartialVipss = FLAGS_initPV;
-
-        if(FLAGS_output.size() > 0)
+        LocalVipss::InitWithPartialVipss = args.initPV;
+        vipss_unit.is_surfacing_ = args.surfacing;
+        if(args.output.size() > 0)
         {
-            vipss_unit.is_surfacing_ = FLAGS_surfacing;
             std::string out_path, out_filename, out_extname;
-            SplitFileName(FLAGS_output, out_path, out_filename, out_extname);
-
+            SplitFileName(args.output, out_path, out_filename, out_extname);
             vipss_unit.out_dir_ = out_path;
             vipss_unit.file_name_ = out_filename;
 
@@ -53,7 +69,7 @@ int main(int argc, char** argv)
             // std::cout << "out_filename : " << out_filename << std::endl;
             // std::cout << "out_extname : " << out_extname << std::endl;
             vipss_unit.out_normal_path_ = out_path + "/" + out_filename + "_out_normal";
-            vipss_unit.out_surface_path_ = FLAGS_output;
+            vipss_unit.out_surface_path_ = args.output;
             vipss_unit.out_debug_path_ = out_path + "/" + out_filename + "_debug.txt";
         } else {
             vipss_unit.out_dir_ = in_path;
@@ -62,9 +78,9 @@ int main(int argc, char** argv)
             vipss_unit.out_surface_path_ = in_path + "/" + in_filename + "_out_surface";
             vipss_unit.out_debug_path_ = in_path + "/" + in_filename + "_debug.txt";
         }
-        vipss_unit.user_lambda_ = FLAGS_lambda;
+        vipss_unit.user_lambda_ = args.lambda;
         vipss_unit.use_hrbf_surface_ = true;
-        vipss_unit.volume_dim_ = FLAGS_volumeDim; 
+        vipss_unit.volume_dim_ = args.volumeDim; 
         vipss_unit.axi_plane_ = AXI_PlANE::XYZ;
         vipss_unit.hrbf_type_ = HRBF_SURFACE_TYPE::LOCAL_HRBF_NN;
         // vu.hrbf_type_ = HRBF_SURFACE_TYPE::GLOBAL_HRBF;
@@ -74,7 +90,6 @@ int main(int argc, char** argv)
     }
     return 0;
 }
-
 
 void SplitFileName (const std::string& fullfilename,std::string &filepath,std::string &filename,std::string &extname) {
     size_t pos;
